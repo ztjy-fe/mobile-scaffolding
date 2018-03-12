bd68fcabc1f2fd2080ba6f679574774e98e21472
ztjy-fe <zhangzhenglong@szy.cn>
2018-03-12 17:40:15 +0800

    update

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -2,30 +2,39 @@
 
 module.exports = {
 	root: true,
+	// 指定eslint解析器，解析器必须符合规则，babel-eslint解析器是对babel解析器的包装使其与ESLint解析
 	parserOptions: {
 		parser: 'babel-eslint'
 	},
+	// 指定环境的全局变量，下面的配置指定为浏览器环境
 	env: {
 		browser: true,
 	},
+	// 配置标准的js风格
 	extends: [
 		// https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
 		// consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
 		'plugin:vue/essential', 
-		// https://github.com/standard/standard/blob/master/docs/RULES-en.md
+		// 详细说明：https://github.com/standard/standard/blob/master/docs/RULES-zhcn.md
 		'standard'
 	],
-	// required to lint *.vue files
+	// 规范 *.vue 文件
 	plugins: [
 		'vue'
 	],
-	// add your custom rules here
+    // 下面这些rules是用来设置从插件来的规范代码的规则
+    // 主要有如下的设置规则，可以设置字符串也可以设置数字，两者效果一致
+    // "off" -> 0 关闭规则
+    // "warn" -> 1 开启警告规则
+    // "error" -> 2 开启错误规则
 	rules: {
-		// allow async-await
+		// 允许方法之间加星号，如ES6提供的生成器函数 function * generator() {}
 		'generator-star-spacing': 'off',
-		// allow debugger during development
+		// 许在开发环境下使用debugger
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		// tab 缩进
 		"indent": ["error", "tab"],
+		// 禁用 tab，如果你已经建立了好的使用 tab 的标准，可以不启用此规则。
 		"no-tabs":"off"
 	}
 }
--- template/build/build.js
+++ template/build/build.js
@@ -30,7 +30,8 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
 		process.stdout.write(stats.toString({
 			colors: true,
 			modules: false,
-			children: false, // 使用ts-loader插件时, 设置为true，构建过程中会显示TypeScript errors.
+			// 使用ts-loader插件时, 设置为true，构建过程中会显示TypeScript errors.
+			children: false,
 			chunks: false,
 			chunkModules: false
 		}) + '\n\n')
